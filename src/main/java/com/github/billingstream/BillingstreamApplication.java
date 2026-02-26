5a1d434d4180726c94ac1b92eab4db6402bdc5b2
vatsan127 <vatsan127@gmail.com>
2026-02-26 21:39:08 +0530

    first commit

---
+++ src/main/java/com/github/billingstream/BillingstreamApplication.java
@@ -0,0 +1,13 @@
+package com.github.billingstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/billingstream/BillingstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.billingstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
